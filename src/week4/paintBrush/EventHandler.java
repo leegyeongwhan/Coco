b538b274422e9f4946710c75ec19d8249cb3548b
lee <leekhy02@naver.com>
2021-11-25 02:24:55 +0900

    feat: 이벤트 헨들러 무한루프수정

--- src/week4/paintBrush/EventHandler.java
+++ src/week4/paintBrush/EventHandler.java
@@ -5,24 +5,31 @@ import java.awt.event.*;
 
 class EventHandler extends MouseMotionAdapter implements ActionListener {
     MyCanvas can = new MyCanvas();
-    MyFrame f = new MyFrame();
+    String red,green,blue,selectRemove,allRemove;
+    EventHandler(Frame f){
+
+    }
+    EventHandler(){
+
+    }
     public void mouseDragged(MouseEvent e) {
         can.x = e.getX();
         can.y = e.getY();
         can.repaint();
     }
+
     public void actionPerformed(ActionEvent e) {  //클릭시
         Object o = e.getSource();
         can.flag = 0;//flag를 0으로 주어 그리기가 기본작업이 되게 한다.
-        if (o == f.red) {
+        if (o == red) {
             can.cr = Color.red;
-        } else if (o == f.green) {
+        } else if (o == green) {
             can.cr = Color.green;
-        } else if (o == f.blue) {
+        } else if (o == blue) {
             can.cr = Color.blue;
-        } else if (o == f.selectRemove) {
+        } else if (o == selectRemove) {
             can.cr = can.getBackground(); //배경색을 칠해서 지우기
-        } else if (o == f.allRemove) {
+        } else if (o == allRemove) {
             can.flag = 1;
             can.repaint();
         }
--- src/week4/paintBrush/MyFrame.java
+++ src/week4/paintBrush/MyFrame.java
@@ -1,31 +1,30 @@
 package week4.paintBrush;
 
+import week4.paintBrush.MyCanvas;
+
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.List;
 
 public class MyFrame extends Frame {
     Button red, green, blue;
-    Button allRemove,selectRemove;
-    Button square,circle,beeline,curve;
+    Button allRemove, selectRemove;
+    Button square, circle, beeline, curve;
+
     public MyFrame() {
-        Frame f = new Frame("그림판");
-        f.setSize(200, 200);
-        Panel figure = new Panel();
+        super("그림판");
+        Frame f = new Frame();
+        Panel figurePanel = new Panel();
         Panel removePanel = new Panel();
         Panel colorPanel = new Panel();
         Panel center = new Panel();
         // 캔버스 부착
-//        MyCanvas mc = new MyCanvas();
-//        center.add(mc);
-        //    Panel menuPanel = new Panel();
-        figure.setLayout(new GridLayout(2, 2));
+        figurePanel.setLayout(new GridLayout(2, 2));
         removePanel.setLayout(new GridLayout(1, 1));
 // Frame BorderLayout 은 기본적으로 로 설정되어 있으므로 따로 설정하지 않아도 됨
         f.setLayout(new BorderLayout());
         Button north = new Button("North");
-        Button south = new Button("South");
-
         MenuBar mb = new MenuBar();
         Menu mEdit = new Menu("NEW");
         Menu mView = new Menu("OPEN");
@@ -35,18 +34,24 @@ public class MyFrame extends Frame {
         mb.add(mHelp);
 
 //---------------------west-----------------------
-        square = new Button("Square");
-         circle = new Button("circle");
-         beeline = new Button("beeline");
-        curve = new Button("curve");
-        figure.add(square);
-        figure.add(circle);
-        figure.add(beeline);
-        figure.add(curve);
-        Button west = new Button("West");
+        square = new Button("사각형");
+        circle = new Button("원");
+        beeline = new Button("직선");
+        curve = new Button("곡선");
+
+        square.addActionListener(new EventHandler());
+        circle.addActionListener(new EventHandler());
+        beeline.addActionListener(new EventHandler());
+        curve.addActionListener(new EventHandler());
+        figurePanel.add(square);
+        figurePanel.add(circle);
+        figurePanel.add(beeline);
+        figurePanel.add(curve);
+
+
         //-------------------------------------------
         //-------east--------------
-         allRemove = new Button("allRemove");
+        allRemove = new Button("allRemove");
         selectRemove = new Button("selectRemove");
         removePanel.add(allRemove);
         removePanel.add(selectRemove);
@@ -55,35 +60,48 @@ public class MyFrame extends Frame {
         red = new Button("RED");
         green = new Button("GREEN");
         blue = new Button("BLUE");
+
+        red.addActionListener(new EventHandler());
+        green.addActionListener(new EventHandler());
+        blue.addActionListener(new EventHandler());
         colorPanel.add(red);
         colorPanel.add(green);
         colorPanel.add(blue);
         //--------------------------------------
-        // Button center = new Button("Center");
+
+//        MyCanvas can = new MyCanvas();
+//        can=new MyCanvas();
+//        can.setSize(300,300);
+//        can.setBackground(Color.white);
+//        center.add(can);
+
+
 // Frame 5 Button . 의 개의 각 영역에 을 하나씩 추가한다
         f.add(north, "North"); // f.add("North",north); . 와 같이 쓸 수도 있다
         f.add(colorPanel, "South"); // South의 대소문자 정확히
         f.add(removePanel, "East"); // East , BorderLayout.EAST 대신 사용가능
-        f.add(figure, "West");
+        f.add(figurePanel, "West");
         f.add(center, "Center");
+
+
+        //   f.setVisible(true);
+        f.setSize(500, 500);
         f.setVisible(true);
-        //-------------------------------켄버스
+//        -------------------------------켄버스
 
 
-///------리스너 부착----------
-        EventHandler eh = new EventHandler();
-        square.addActionListener(eh);
-        circle.addActionListener(eh);
-        beeline.addActionListener(eh);
-        curve.addActionListener(eh);
+//        EventHandler eh = new EventHandler(f);
 
-        red.addActionListener(eh);
-        green.addActionListener(eh);
-        blue.addActionListener(eh);
-
-        allRemove.addActionListener(eh);
-        selectRemove.addActionListener(eh);
-
+//        circle.addActionListener(eh);
+//        beeline.addActionListener(eh);
+//        curve.addActionListener(eh);
+//
+//        red.addActionListener(eh);
+//        green.addActionListener(eh);
+//        blue.addActionListener(eh);
+//
+//        allRemove.addActionListener(eh);
+//        selectRemove.addActionListener(eh);
 
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
