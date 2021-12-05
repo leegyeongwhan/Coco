2ffecfeed1261c521a6dd8d90a0d53d7c260f09c
lee <leekhy02@naver.com>
2021-12-06 01:14:04 +0900

    feat: 사용자 입력 숫자 입력받기 구현

--- src/baseball/Game.java
+++ src/baseball/Game.java
@@ -4,32 +4,35 @@ import java.util.*;
 
 public class Game {
 
-    private void inputNumber() {
+
+
+    private String inputNumber() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("숫자를 입력해주세요 ex)123 :");
-        int checkInputNumber = scanner.next().length();
-        while (checkInputNumber != 3) {
+        String inputNumber = scanner.next();
+        while (inputNumber.length() != 3) {
             System.out.println("다시 입력해주세요");
+            inputNumber = scanner.next();
         }
-
+        return inputNumber;
     }
 
-    private void checkInputNumber() {
-
+    private String[] checkInputNumber() {
+        String[] checkNumber = inputNumber().split("");
+        return checkNumber;
     }
 
 
-
-    private List<Integer> computerNumberSuffle() {
-        Integer[] numberArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        List<Integer> list = Arrays.asList(numberArray);
+    private List<String> computerNumberSuffle() {
+        String[] numberArray = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        List<String> list = Arrays.asList(numberArray);
         Collections.shuffle(list);
         return list;
     }
 
-    private int[] checkComputerNumber() {
-        int[] computerNumberList = new int[3];
-        List<Integer> list;
+    private String[] checkComputerNumber() {
+        String[] computerNumberList = new String[3];
+        List<String> list;
         list = computerNumberSuffle();
         for (int i = 0; i < computerNumberList.length; i++) {
             computerNumberList[i] = list.get(i);
@@ -38,14 +41,17 @@ public class Game {
     }
 
 
-    private void player() {
-        inputNumber();
-
-    }
-
 
     public void startGame() {
-        checkComputerNumber();  //123sd
-      //  System.out.println(Arrays.toString(checkComputerNumber()));
+        //  System.out.println(Arrays.toString(checkComputerNumber()));
+        String[] checkPlNum = checkInputNumber();
+        String[] checkComNum = checkComputerNumber();
+        resultGame(checkPlNum,checkComNum);
+        System.out.println(Arrays.toString(checkComNum));
+        System.out.println(Arrays.toString(checkPlNum));
+    }
+
+    private void resultGame(String[] checkPlNum, String[] checkComNum) {
+
     }
 }
